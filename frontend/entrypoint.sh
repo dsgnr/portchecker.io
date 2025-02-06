822f301e3f7f058ddb8710b02e0f59a913e1f268
dsgnr <email@danielhand.io>
2025-02-06 21:09:11 +0000

    Add envsubst to nginx config to allow API_URL override

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -e
 
+# Export API_URL so envsubst can use it
+: ${API_URL:="http://api:8000"}
+export API_URL
+
 # `DEFAULT_PORT` is set by Webpack at container build time if the environment variable is provided.
 # If this variable is not set at that time (like for production images), we must modify the rendered HTML on container up.
 if [ -n "$DEFAULT_PORT" ]; then
@@ -12,5 +16,8 @@ if [ -n "$DEFAULT_PORT" ]; then
 else
     echo "DEFAULT_PORT is not set. No changes made."
 fi
+
+# Updates the `API_URL` variable in the Nginx config. Defaults to http://api:8000 if not set.
+envsubst "\$API_URL" < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
