8e84bf01e902d3dcd40ff8e97148d10f8d0dc26b
dsgnr <email@danielhand.io>
2025-05-22 19:42:58 +0100

    Allow default host to be defined

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -5,6 +5,18 @@ set -e
 : ${API_URL:="http://api:8000"}
 export API_URL
 
+# `DEFAULT_HOST` is set by Webpack at container build time if the environment variable is provided.
+# If this variable is not set at that time (like for production images), we must modify the rendered HTML on container up.
+if [ -n "$DEFAULT_HOST" ]; then
+    if sed -i -E "s/(<input[^>]*id=\"host\"[^>]*value=\")[^\"]*\"/\\1${DEFAULT_HOST}\"/" /usr/share/nginx/html/index.html; then
+        echo "Updated DEFAULT_HOST value to $DEFAULT_HOST."
+    else
+        echo "An error occurred when attempting to set the DEFAULT_HOST value."
+    fi
+else
+    echo "DEFAULT_HOST is not set. No changes made."
+fi
+
 # `DEFAULT_PORT` is set by Webpack at container build time if the environment variable is provided.
 # If this variable is not set at that time (like for production images), we must modify the rendered HTML on container up.
 if [ -n "$DEFAULT_PORT" ]; then
--- frontend/web/webpack.config.js
+++ frontend/web/webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
             minify: {
                 collapseWhitespace: true,
             },
+            DEFAULT_HOST: process.env.DEFAULT_HOST,
             DEFAULT_PORT: process.env.DEFAULT_PORT,
             GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
         }),
